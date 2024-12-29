a94e030b9d40d1f525b75638bb13d32a1f489141
Hoang An Unicode <contact@unicode.vn>
2024-12-29 23:00:16 +0700

    Update Login Page UI

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,9 +1,36 @@
-import { Outlet } from "react-router-dom";
-
+import { Link, Outlet } from "react-router-dom";
+import authBg from "./images/THmkXhzz2_a.avif";
 export default function AuthLayout() {
   return (
-    <div className="w-full px-10">
-      <Outlet />
+    <div
+      style={
+        {
+          "--image-url": `url(${authBg})`,
+        } as React.CSSProperties
+      }
+      className={`min-h-screen flex items-center justify-center bg-no-repeat bg-none lg:bg-[center_-50%] lg:bg-[length:120%] lg:bg-[image:var(--image-url)]`}
+    >
+      <div className="w-[418px] mx-auto px-[24px] pt-[15vh]">
+        <Outlet />
+      </div>
+      <ul className="flex gap-2 justify-center absolute bottom-4 left-0 right-0 text-[13px] text-[gray]">
+        <li>© 2024</li>
+        <li>
+          <Link to={"#"} className="hover:underline">
+            Threads Terms
+          </Link>
+        </li>
+        <li>
+          <Link to={"#"} className="hover:underline">
+            Privacy Policy
+          </Link>
+        </li>
+        <li>
+          <Link to={"#"} className="hover:underline">
+            Cookies Policy
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,3 +1,59 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+import { Separator } from "@/components/ui/separator";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h1 className="text-center text-black font-[700] mb-[20px]">
+        Log in with your Instagram account
+      </h1>
+      <form>
+        <Input
+          type="text"
+          placeholder="Username, phone or email"
+          className="mb-5 bg-[rgb(245,245,245)] py-6"
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          className="mb-5 bg-[rgb(245,245,245)] py-6"
+        />
+        <Button
+          size={null}
+          className="w-full py-4 disabled:opacity-100 disabled:text-[gray]"
+          disabled
+        >
+          Log in
+        </Button>
+      </form>
+      <div className="flex justify-center mt-[20px]">
+        <Link to="/forgot-password" className="text-[#999]">
+          Forgot password?
+        </Link>
+      </div>
+      <div className="flex mt-[20px] gap-3 items-center justify-center text-[#999]">
+        <Separator className="w-[20px]" />
+        <span>or</span>
+        <Separator className="w-[20px]" />
+      </div>
+      <div className="flex justify-between gap-5 mt-[20px]">
+        <Button variant={"outline"} className="w-full">
+          <FaFacebookSquare /> Facebook
+        </Button>
+        <Button variant={"outline"} className="w-full">
+          <FaGoogle /> Google
+        </Button>
+      </div>
+      <div className="flex justify-center mt-[20px] gap-3">
+        <span>Have not an account?</span>
+        <Link to="/register" className="text-[#999]">
+          Register now
+        </Link>
+      </div>
+    </div>
+  );
 }
