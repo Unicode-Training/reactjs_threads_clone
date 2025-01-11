5a303c7030545ef1dd21072acc5db18c03b59133
Hoang An Unicode <hoangan.web@gmail.com>
2025-01-11 22:06:34 +0700

    Validate Register - Phan 1

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -5,111 +5,162 @@ import { Separator } from "@/components/ui/separator";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaGoogle } from "react-icons/fa";
 import { useForm } from "react-hook-form";
-import { useEffect, useState } from "react";
 import { MESSAGES } from "@/constants/message";
 import { RouteNames } from "@/constants/route";
+import { useToast } from "@/hooks/use-toast";
+type Inputs = {
+  name: string;
+  email: string;
+  password: string;
+  confirm_password: string;
+  phone: string;
+  username: string;
+};
+
 export default function Register() {
-  const [isDisabled, setDisabled] = useState(true);
   const {
     register,
     handleSubmit,
-    trigger,
     formState: { errors },
-  } = useForm({
-    mode: "onChange",
-  });
+  } = useForm<Inputs>();
+  const { toast } = useToast();
   const onSubmit = async (formData: unknown) => {
     console.log(formData);
   };
-  useEffect(() => {
-    trigger(["username", "password"], {
-      shouldFocus: true,
+  const handleErrors = () => {
+    toast({
+      title: "Please fill all the fields",
     });
-    setDisabled(false);
-  }, []);
+  };
 
   return (
     <div>
       <h1 className="text-center text-black font-[700] mb-[20px]">
         Register an account
       </h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit, handleErrors)}>
         <div className="grid grid-cols-2 gap-4">
           <div>
-            <Input
-              type="text"
-              placeholder="Display Name"
-              className="mb-5 bg-[rgb(245,245,245)] py-6"
-              {...register("name", {
-                required: {
-                  value: true,
-                  message: MESSAGES.AUTH.USERNAME_INVALID,
-                },
-              })}
-            />
-            <Input
-              type="email"
-              placeholder="Email"
-              className="mb-5 bg-[rgb(245,245,245)] py-6"
-              {...register("email", {
-                required: {
-                  value: true,
-                  message: MESSAGES.AUTH.USERNAME_INVALID,
-                },
-              })}
-            />
-            <Input
-              type="text"
-              placeholder="Phone"
-              className="mb-5 bg-[rgb(245,245,245)] py-6"
-              {...register("phone", {
-                required: {
-                  value: true,
-                  message: MESSAGES.AUTH.USERNAME_INVALID,
-                },
-              })}
-            />
+            <div className="mb-5">
+              <Input
+                type="text"
+                placeholder="Display Name"
+                className=" bg-[rgb(245,245,245)] py-6"
+                {...register("name", {
+                  required: {
+                    value: true,
+                    message: MESSAGES.AUTH.USERNAME_INVALID,
+                  },
+                  minLength: {
+                    value: 2,
+                    message: "Name must be at least 2 characters",
+                  },
+                })}
+              />
+              {errors.name && (
+                <span className="text-sm text-[red]">
+                  {errors.name.message}
+                </span>
+              )}
+            </div>
+            <div className="mb-5">
+              <Input
+                type="email"
+                placeholder="Email"
+                className="bg-[rgb(245,245,245)] py-6"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: MESSAGES.AUTH.USERNAME_INVALID,
+                  },
+                })}
+              />
+              {errors.email && (
+                <span className="text-sm text-[red]">
+                  {errors.email.message}
+                </span>
+              )}
+            </div>
+
+            <div className="mb-5">
+              <Input
+                type="text"
+                placeholder="Phone"
+                className="bg-[rgb(245,245,245)] py-6"
+                {...register("phone", {
+                  required: {
+                    value: true,
+                    message: MESSAGES.AUTH.USERNAME_INVALID,
+                  },
+                })}
+              />
+              {errors.phone && (
+                <span className="text-sm text-[red]">
+                  {errors.phone.message}
+                </span>
+              )}
+            </div>
           </div>
           <div>
-            <Input
-              type="text"
-              placeholder="Username"
-              className="mb-5 bg-[rgb(245,245,245)] py-6"
-              {...register("username", {
-                required: {
-                  value: true,
-                  message: MESSAGES.AUTH.USERNAME_INVALID,
-                },
-              })}
-            />
-            <Input
-              type="password"
-              placeholder="Password"
-              className="mb-5 bg-[rgb(245,245,245)] py-6"
-              {...register("password", {
-                required: {
-                  value: true,
-                  message: MESSAGES.AUTH.PASSWORD_INVALID,
-                },
-              })}
-            />
-            <Input
-              type="password"
-              placeholder="Confirm Password"
-              className="mb-5 bg-[rgb(245,245,245)] py-6"
-              {...register("confirm_password", {
-                required: {
-                  value: true,
-                  message: MESSAGES.AUTH.PASSWORD_INVALID,
-                },
-              })}
-            />
+            <div className="mb-5">
+              <Input
+                type="text"
+                placeholder="Username"
+                className="bg-[rgb(245,245,245)] py-6"
+                {...register("username", {
+                  required: {
+                    value: true,
+                    message: MESSAGES.AUTH.USERNAME_INVALID,
+                  },
+                })}
+              />
+              {errors.username && (
+                <span className="text-sm text-[red]">
+                  {errors.username.message}
+                </span>
+              )}
+            </div>
+            <div className="mb-5">
+              <Input
+                type="password"
+                placeholder="Password"
+                className=" bg-[rgb(245,245,245)] py-6"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: MESSAGES.AUTH.PASSWORD_INVALID,
+                  },
+                })}
+              />
+              {errors.password && (
+                <span className="text-sm text-[red]">
+                  {errors.password.message}
+                </span>
+              )}
+            </div>
+            <div className="mb-5">
+              <Input
+                type="password"
+                placeholder="Confirm Password"
+                className=" bg-[rgb(245,245,245)] py-6"
+                {...register("confirm_password", {
+                  required: {
+                    value: true,
+                    message: MESSAGES.AUTH.PASSWORD_INVALID,
+                  },
+                })}
+              />
+              {errors.confirm_password && (
+                <span className="text-sm text-[red]">
+                  {errors.confirm_password.message}
+                </span>
+              )}
+            </div>
           </div>
         </div>
         <Button
           size={null}
           className="w-full py-4 disabled:opacity-100 disabled:text-[gray]"
-          disabled={isDisabled || Object.keys(errors).length > 0}
         >
           Register
         </Button>
